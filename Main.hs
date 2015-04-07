8c5c48b9a32fc81721dd4b5a95417b67a78e884f
kqr <k@rdw.se>
2015-04-07 17:47:08 +0200

    made it possible to link stuff

--- Main.hs
+++ Main.hs
@@ -1,29 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Prelude hiding (readFile, takeWhile, null, length, lines, unlines)
+import Prelude hiding (takeWhile, words)
+import System.IO (hPutStrLn, stderr)
 import Data.Ord (comparing)
-import Data.List (sort, groupBy)
+import Data.Char (isSpace)
+import Data.List (sort)
 import Data.Monoid ((<>))
-import Control.Applicative ((*>), (<*), (<$>), many)
-import Control.Monad (forM_, unless)
-import System.Environment (getArgs)
+import Control.Applicative ((*>), (<*), (<|>), many, optional, liftA2)
+import Control.Monad (forM_, unless, void)
 
-import Data.Text (Text, strip, null, intercalate, length, lines, unlines, splitOn)
-import Data.Text.IO (readFile)
-import qualified Data.Text.Lazy.IO as T
+import Data.Text (Text, strip, intercalate, toLower, words)
+import qualified Data.Text as T (null)
+import qualified Data.Text.IO as T (getContents)
+import qualified Data.Text.Lazy.IO as TL (putStrLn)
 
 import Data.Attoparsec.Text
 
+import Text.Blaze (ToMarkup, toMarkup, preEscapedToMarkup, toValue)
 import Text.Blaze.Html.Renderer.Text (renderHtml)
-import Text.Blaze.Html5 (Html, toHtml, preEscapedToHtml, p, ul, li, strong)
+import Text.Blaze.Html5 (Html, toHtml, preEscapedToHtml, p, ul, li, strong, a, (!))
+import Text.Blaze.Html5.Attributes (href, name)
+
 
 
 main = do
-  [fn] <- getArgs
-  defs <- readFile fn
+  defs <- T.getContents
   case parseOnly dict defs of
-    Right dict -> T.putStrLn (renderHtml (htmlDict (sort dict)))
-    Left err -> putStrLn err
+    Right entries -> TL.putStrLn (renderHtml (htmlDict (sort entries)))
+    Left err -> hPutStrLn stderr err
 
 
 
@@ -31,47 +35,60 @@ main = do
 
 data Entry = Entry
   { _word :: Text
-  , _definition :: [Text]
-  , _alternatives :: [(Text, Text)]
+  , _definition :: [[RefText]]
+  , _alternatives :: [(Text, [RefText])]
   } deriving Show
 
-type RefText = [Either Text Text]
+data RefText = Reference Text | Regular Text deriving Show
 
 instance Eq  Entry where e1 == e2 = _word e1 == _word e2
 instance Ord Entry where compare = comparing _word
 
 
+
 -- PARSING
 
 dict :: Parser [Entry]
-dict = manyTill (skipSpace *> entry <* skipSpace) endOfInput
+dict = skipSpace *> manyTill entry endOfInput
 
 entry :: Parser Entry
 entry = do
-  word <- fmap normalise (pureText <* char '{')
-  def <- fmap (splitOn "\n\n") pureText
-  alts <- many (alternative <* skipSpace) <* char '}'
+  word <- pureLine <* openBlock
+  def <- many paragraph
+  alts <- many alternative
+  closeBlock
   return (Entry word def alts)
 
-alternative :: Parser (Text, Text)
+alternative :: Parser (Text, [RefText])
 alternative = do
-  name <- char '|' *> pureText <* char '{'
-  desc <- pureText <* char '}'
+  name <- char '|' *> pureLine <* openBlock
+  desc <- option [] paragraph
+  closeBlock
   return (name, desc)
 
---linkText :: Parser RefText
---linkText = many (fmap Left link <|> fmap Right pureText)
+openBlock :: Parser ()
+openBlock = char '{' >> skipSpace
 
---link :: Parser Text
---link = fmap normalise (string "`{" *> pureText <* char '}')
+closeBlock :: Parser ()
+closeBlock = char '}' >> skipSpace
 
-pureText :: Parser Text
-pureText = fmap cleanedText (takeWhile (notInClass "{`|}"))
-  where cleanedText = strip . unlines . compactLF . map strip . lines
-        compactLF = map (!!0) . groupBy (\a b -> null (a <> b))
+paragraph :: Parser [RefText]
+paragraph = liftA2 (:) linkText rest <* skipSpace
+  where rest = option [] (void (optional (char '\n')) *> paragraph)
 
-normalise :: Text -> Text
-normalise = intercalate " " . lines
+linkText :: Parser RefText
+linkText = fmap Reference pureLink <|> fmap Regular pureLine
+
+pureLink :: Parser Text
+pureLink = do
+  takeWhile (\c -> isSpace c && c /= '\n')
+  string "`{" *> pureLine <* char '}'
+
+pureLine :: Parser Text
+pureLine = do 
+  text <- fmap strip (takeWhile (notInClass "{`|}\n"))
+  if T.null text then fail "empty line" else return text
+
 
 
 -- HTML GENERATION
@@ -81,12 +98,25 @@ htmlDict dict = ul (forM_ dict (li . htmlEntry))
 
 htmlEntry :: Entry -> Html
 htmlEntry (Entry word (first:rest) alts) = do
+  a ! name (toValue (slugify word)) $ ""
   p (strong (toHtml (word <> ": ")) >> preEscapedToHtml first)
   forM_ rest (p . preEscapedToHtml)
   ul (forM_ alts htmlAlternative)
 
-htmlAlternative :: (Text, Text) -> Html
-htmlAlternative (name, desc) = li . p . toHtml $
-  if null desc
-     then name
-     else name <> " (" <> desc <> ")"
+htmlAlternative :: (Text, [RefText]) -> Html
+htmlAlternative (name, desc) = li . p $ do
+  toHtml name
+  unless (null desc) (" (" >> toHtml desc >> ")")
+
+instance ToMarkup RefText where
+  toMarkup (Reference text) = a ! href (toValue ("#" <> slugify text)) $ preEscapedToMarkup text
+  toMarkup (Regular text) = preEscapedToMarkup text
+
+instance ToMarkup a => ToMarkup [a] where
+  toMarkup [] = ""
+  toMarkup [x] = toMarkup x
+  toMarkup (x:xs) = toMarkup x <> " " <> toMarkup xs
+
+slugify :: Text -> Text
+slugify = intercalate "-" . words . toLower
+
